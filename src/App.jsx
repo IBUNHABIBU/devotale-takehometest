6390d73245382c6124470457a1f39bbb00cbb980
IBUNHABIBU <ibunhabib@gmail.com>
2022-12-24 19:44:46 +0300

    Add Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { createContext, useState } from 'react';
+import Home from './component/Home';
+
+export const PostContext = createContext(null);
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [post, setPost] = useState(PostContext);
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <PostContext.Provider value={{ post, setPost }}>
+      <div className="container">
+
+        <Home />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    </PostContext.Provider>
+  );
 }
 
-export default App
+export default App;
