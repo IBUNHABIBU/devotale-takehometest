5c86470cac15369d8f9722ee8835be69b56a344b
IBUNHABIBU <ibunhabib@gmail.com>
2022-12-24 21:12:14 +0300

    Add home page'

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,31 +1,34 @@
 import React, { useContext, useEffect } from 'react';
 import axios from 'axios';
-import { AppContext } from '../App';
+import { PostContext } from '../App';
+
 
 const Home = () => {
-  const { post, setPost } = useContext(AppContext);
-
+  const { post, setPost } = useContext(PostContext);
+    const API_KEY = 'YSCHbutcdsXn1Nal3CH0HvxD7TWdWTmZ';
   useEffect(() => {
-    axios.get('http://localhost:3000/posts')
-      .then((response) => {
+    axios.get(`https://api.giphy.com/v1 /clips/trending?api_key=${API_KEY}&limit=10`)
+     .then((response) => {
         setPost(response.data);
+        console.log(response.data);
       });
   }, [setPost]);
 
   return (
     <div className="container border border-info">
       <div className="row">
+        hello
         {
-                Array.from(post).map((data) => (
-                  <div className="col-3" key={data.id}>
-                    <div className="card">
-                      <img className="card-img-top" src={data.image} alt="url for foto" />
-                      <div className="card-body">
-                        <p className="card-text">{data.caption}</p>
-                      </div>
-                    </div>
-                  </div>
-                ))
+                // Array.from(post).map((data) => (
+                //   <div className="col-3" key={data.id}>
+                //     <div className="card">
+                //       <img className="card-img-top" src={data.image} alt="url for foto" />
+                //       <div className="card-body">
+                //         <p className="card-text">{data.caption}</p>
+                //       </div>
+                //     </div>
+                //   </div>
+                // ))
             }
       </div>
     </div>
