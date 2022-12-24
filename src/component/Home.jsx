5a03b45da6285c0117c06a825813fc06dcfe5fb0
IBUNHABIBU <ibunhabib@gmail.com>
2022-12-24 19:47:23 +0300

    Add Home page

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import axios from 'axios';
+import { AppContext } from '../App';
+import Loading from './Loading';
 
-const Home = () => (
-  <div>Home</div>
-);
+const Home = () => {
+  const { post, setPost } = useContext(AppContext);
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/posts')
+      .then((response) => {
+        setPost(response.data);
+      });
+  }, [setPost]);
+
+  return (
+    <div className="container border border-info">
+      <div className="row">
+        {
+                Array.from(post).map((data) => (
+                  <div className="col-3" key={data.id}>
+                    <div className="card">
+                      <img className="card-img-top" src={data.image} alt="url for foto" />
+                      <div className="card-body">
+                        <p className="card-text">{data.caption}</p>
+                      </div>
+                    </div>
+                  </div>
+                ))
+            }
+      </div>
+    </div>
+  );
+};
 
 export default Home;
